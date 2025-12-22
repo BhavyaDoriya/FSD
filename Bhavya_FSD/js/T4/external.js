49c9de8b75f9c54be981e9734e36a83c66670c6b
Bhavya <bhavyadoriya2007@gmail.com>
2025-12-22 12:11:48 +0530

    External and internal js

---
+++ Bhavya_FSD/js/T4/external.js
@@ -0,0 +1,5 @@
+document.writeln("This is blah blah blah")
+document.writeln("This is blah blah blah<br>")
+document.writeln("This is blah blah blah")
+// Write ln adds space after each line
+// br adds a line break without extra space
